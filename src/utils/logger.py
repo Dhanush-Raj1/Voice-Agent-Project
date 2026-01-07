6da33289a852a7480b78aebd3cec1cdb672ccd91
Dhanush <dhanush1004raj@gmail.com>
2026-01-07 18:41:14 +0530

    cleaned logger

--- src/utils/logger.py
+++ src/utils/logger.py
@@ -4,10 +4,10 @@ import sys
 def setup_logger(name: str = "voice_agent", level=logging.INFO):
     """Setup and return a logger"""
     logger = logging.getLogger(name)
-    logger.setLevel(level)
+    logger.setLevel(level)                        # logger level info 
     
     # Console handler
-    handler = logging.StreamHandler(sys.stdout)
+    handler = logging.StreamHandler(sys.stdout)    # send logs to the console
     handler.setLevel(level)
     
     # Formatter
@@ -18,7 +18,7 @@ def setup_logger(name: str = "voice_agent", level=logging.INFO):
     handler.setFormatter(formatter)
     
     # Add handler if not already added
-    if not logger.handlers:
-        logger.addHandler(handler)
+    if not logger.handlers:                       # setup_logger will be called multiple times in the code base, 
+        logger.addHandler(handler)                # without this check, multiple handlers will be added, duplicate logs appear.
     
     return logger
